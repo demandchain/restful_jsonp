1a4755feed679981754e4a7b26a08bda68102317
Jeshua Borges <jesh@homerun.com>
2011-11-28 08:03:59 -0800

    Remove initialize message

--- lib/restful_jsonp.rb
+++ lib/restful_jsonp.rb
@@ -6,7 +6,6 @@ require 'restful_jsonp/jsonp_responder'
 module RestfulJSONP
   class Railtie < Rails::Railtie
     initializer "restful_jsonp.replace_rack_methodoverride" do |app|
-      Rails.logger.debug "Swapping in RestfulJSONP Middleware"
       app.middleware.swap Rack::MethodOverride, RestfulJSONP::MethodOverride
     end
   end
